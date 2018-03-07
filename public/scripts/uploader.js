10edf6f44a8c5cbb7bc2a448058405606bd8911f
Tobias Schaffer <schaffer.tobias@gmail.com>
2018-03-07 09:59:38 +0100

    handle autofill profiles with key

--- public/scripts/uploader.js
+++ public/scripts/uploader.js
@@ -149,7 +149,7 @@ friendlyPix.Uploader = class {
 
 	})
 	
-	this.initLinkProfilesInputAutocomplete(linkProfilesInput, this.availableProfiles, this);
+	this.initLinkProfilesInputAutocomplete(linkProfilesInput, this.availableProfileIDs, this.availableProfiles, this);
   }
 
 
@@ -325,10 +325,10 @@ friendlyPix.Uploader = class {
   /**
    * If a linked profile is seleted add it above the text field
    */
-  addLinkedProfile(profileFullName) {
+  addLinkedProfile(profileID) {
 	  // create list of linked profiles
-	  linkedProfilesList.innerHTML = linkedProfilesList.innerHTML + "<br>" + profileFullName;
-	  var linkedProfileObject = {display_name: profileFullName, profile_id: this.availableProfileIDs[this.availableProfiles.indexOf(profileFullName)] };
+	  linkedProfilesList.innerHTML = linkedProfilesList.innerHTML + "<br>" + this.availableProfiles[this.availableProfileIDs.indexOf(profileID)];
+	  var linkedProfileObject = {display_name: this.availableProfiles[this.availableProfileIDs.indexOf(profileID)], profile_id: profileID };
 	  this.linkedProfiles.push(linkedProfileObject);
 	  console.log(linkedProfileObject);
 	  console.log(this.linkedProfiles);
@@ -339,7 +339,7 @@ friendlyPix.Uploader = class {
   /**
   * Autocomplete text box for users or animals
   */
-  initLinkProfilesInputAutocomplete(inp, arr, maindoc) {
+  initLinkProfilesInputAutocomplete(inp, arrProfileIDs, arrProfileNames, maindoc) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
@@ -357,16 +357,16 @@ friendlyPix.Uploader = class {
       /*append the DIV element as a child of the autocomplete container:*/
       this.parentNode.appendChild(a);
       /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
+      for (i = 0; i < arrProfileIDs.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        if (arrProfileNames[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
           /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
           /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML = "<strong>" + arrProfileNames[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arrProfileNames[i].substr(val.length);
           /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.innerHTML += "<input type='hidden' value='" + arrProfileIDs[i] + "'>";
           /*execute a function when someone clicks on the item value (DIV element):*/
               b.addEventListener("click", function(e) {
               /*insert the value for the autocomplete text field:*/
